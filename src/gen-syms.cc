d553b08a3edd6b278331596a6df9e7456612ab1e
x022235 <x022235@gmail.com>
2012-09-27 16:07:30 +0900

    shell-execute に :verb 引数を追加 (#330)
    :verb に :runas を指定することで指定したコマンドを管理者権限で起動可能。
    (cherry picked from commit 66c3421ed8e192a1f82482f698325d875c27c2ca)

--- src/gen-syms.cc
+++ src/gen-syms.cc
@@ -1471,6 +1471,7 @@ static symbols kwd[] =
   DEFKWD2 (size-pixel-p),
   DEFKWD2 (code),
   DEFKWD2 (address),
+  DEFKWD2 (verb),
 };
 
 static symbols unint[] =
@@ -2406,7 +2407,7 @@ static symbols ed[] =
   DEFUN3 (set-process-sentinel, 2, 0, 0),
   DEFUN3 (process-sentinel, 1, 0, 0),
   DEFUN3 (process-marker, 1, 0, 0),
-  DEFUN3 (shell-execute, 1, 2, 0),
+  DEFUN3 (shell-execute, 1, 2, FFneed_rest),
   DEFVAR2 (*xyzzyenv-show-flag*),
   DEFVAR2 (*default-process-encoding*),
   DEFVAR2 (*use-shell-execute-ex*),
