4ff74946549db4b1f081bfbb92461dea98245e48
x022235 <x022235@gmail.com>
2014-04-22 14:52:33 +0900

    バッファ選択ダイアログのソート順を改善 (#414)
    [共通設定]-[いろいろ] に以下の設定を追加
    
      * デフォルトのソート方法の指定 (`*buffer-list-sort-type*`)
      * ソート方法の保存 (`*save-buffer-list-sort-flags*`)
    
    ソート方法を保存をチェック場合は前回バッファ選択ダイアログで指定した
    ソート順でソートする。
    
    前回のソート順が保存されていない場合や、保存しないように設定している
    場合はデフォルトのソート順でソートして表示する。
    
    (cherry picked from commit 258577ff34e99851bcdde0d3693f64b893ef71b2)
    
    Conflicts:
    	src/dialogs.cc  : merge by hand
    	src/gen-syms.cc : merge by hand

--- src/gen-syms.cc
+++ src/gen-syms.cc
@@ -1806,6 +1806,10 @@ static symbols ed[] =
   DEFUN3 (get-buffer-colors, 0, 1, 0),
   DEFVAR2 (*change-buffer-colors-hook*),
   DEFVAR2 (*sort-buffer-list-by-created-order*),
+  DEFVAR2 (*buffer-list-sort-ignore-case*),
+  DEFVAR2 (*buffer-list-sort-type*),
+  DEFVAR2 (*save-buffer-list-sort-flags*),
+  DEFVAR2 (*last-buffer-list-sort-flags*),
   DEFVAR2 (*title-bar-text-order*),
   DEFUN3 (set-default-fold-width, 1, 0, 0),
   DEFUN3 (set-buffer-fold-width, 1, 1, 0),
@@ -2757,7 +2761,6 @@ static symbols ed[] =
   DEFVAR2 (*save-window-position*),
   DEFVAR2 (*restore-window-size*),
   DEFVAR2 (*restore-window-position*),
-  DEFVAR2 (*buffer-list-sort-ignore-case*),
   DEFVAR2 (*wow64-enable-file-system-redirector*),
   DEFVAR2 (*parent-process-wow64-p*),
 
